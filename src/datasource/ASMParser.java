909958923d7a2eef49f762d9332e1787ae5b6a8b
rhit-boatmaee <boatmaee@gmail.com>
2022-02-03 15:53:21 -0500

    Added stubs for the adapter ASMParser in datasource.

---
+++ src/datasource/ASMParser.java
@@ -0,0 +1,145 @@
+package datasource;
+
+import org.objectweb.asm.*;
+
+public class ASMParser {
+
+    private ClassReader reader;
+    public ASMParser( ClassReader asm){
+    this.reader = asm;
+    }
+
+    public void readStream(){
+
+    }
+    public void calculateBufferSize(){
+
+    }
+    public void getAccess(){
+
+    }
+    public String getClassName(){
+        return null;
+    }
+    public String getSuperName(){
+        return null;
+    }
+    public String[] getInterfaces(){
+        return null;
+    }
+    public void accept(){
+
+    }
+    private void readModuleAttributes(){
+
+    }
+    private void readRecordComponent(){
+
+    }
+    private void readField(){
+
+    }
+    private void readMethod(){
+
+    }
+    private void readCode(){
+
+    }
+    protected void readLabel(){
+
+    }
+    private void createLabel(){
+
+    }
+    private void createDebugLabel(){
+
+    }
+    private void readTypeAnnotations(){
+
+    }
+    private void getTypeAnnotationsBytecodeOffset(){
+
+    }
+    private void readTypeAnnotationTarget(){
+
+    }
+    private void readParameterAnnotations(){
+
+    }
+    private void readElementValues(){
+
+    }
+    private void readElementValue(){
+
+    }
+    private void computeImplicitFrame(){
+
+    }
+    private void readStackMapFrame(){
+
+    }
+    private void readVerificationTypeInfo(){
+
+    }
+    private void getFirstAttributeOffset(){
+
+    }
+    private void readBootstrapMethodAttributes(){
+
+    }
+    private void readAttribute(){
+
+    }
+    public void getItemCount(){
+
+    }
+    public void getItem(){
+
+    }
+    public void getMaxStringLength(){
+
+    }
+    public void readByte(){
+
+    }
+    public void readUnsignedShort(){
+
+    }
+    public void readShort(){
+
+    }
+    public void readInt(){
+
+    }
+    public void readLong(){
+
+    }
+    public void readUTF8(){
+
+    }
+    public void readUTF(){
+
+    }
+    private String readStringish(){
+        return null;
+    }
+    public void readClass(){
+
+    }
+    public void readModule(){
+
+    }
+    public void readPackage(){
+
+    }
+    public void readConstantDynamic(){
+
+    }
+    public void readConst(){
+
+    }
+
+
+
+
+}
