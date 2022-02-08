f4110c417b88ec2523c43bb3c06198a82f16f772
rhit-hilll <hilll@rose-hulman.edu>
2022-02-08 16:16:08 -0500

    Updated DomainAnalyzer Abstract Class

--- src/domain/DomainAnalyzer.java
+++ src/domain/DomainAnalyzer.java
@@ -4,16 +4,16 @@ package domain;
 public abstract class DomainAnalyzer {
     public int getFeedback(String[] classList) {
         //TODO: Change return types to ReturnType/ParsedData, whatever that ends up being.
-        int x = getRelevantData(classList);
-        analyzeData(x);
+        getRelevantData(classList);
+        analyzeData();
         int y = composeReturnType();
         return y;
     }
 
-    public abstract int getRelevantData(String[] classList);
+    public abstract void getRelevantData(String[] classList);
 
     //TODO: Change x to ParsedData type.
-    public abstract void analyzeData(int x);
+    public abstract void analyzeData();
 
     public abstract int composeReturnType();
 }
