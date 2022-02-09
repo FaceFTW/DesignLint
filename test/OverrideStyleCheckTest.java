55111ce86e4e814ce6e07b3a5c82ca96ab8f6b6c
rhit-mazanyam <mazanyam@rose-hulman.edu>
2022-02-08 20:58:09 -0500

    Setting up Test class for style check

---
+++ test/OverrideStyleCheckTest.java
@@ -0,0 +1,36 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class OverrideStyleCheckTest {
+
+	@Test
+    public void testOverrideUsed() {
+        
+    }
+	
+	@Test
+    public void testOverrideNotUsed() {
+        
+    }
+	
+	@Test
+    public void testNoMethodsOverridden() {
+        
+    }
+	
+	@Test
+    public void testMultipleMethodsOverriddenWithOverride() {
+        
+    }
+	
+	@Test
+    public void testMultipleMethodsOverriddenWithoutOverride() {
+        
+    }
+	
+	@Test
+    public void testMultipleMethodsOverriddenWithAndWithoutOverride() {
+        
+    }
+}
