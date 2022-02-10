4c5263fa5e1c026ab0adddb999bb4352b26cdfc6
rhit-hilll <hilll@rose-hulman.edu>
2022-02-10 17:46:46 -0500

    Edited LinterError

--- src/domain/DomainAnalyzer.java
+++ src/domain/DomainAnalyzer.java
@@ -3,16 +3,13 @@ package domain;
 //Abstract class that all analyzers should extend.
 public abstract class DomainAnalyzer {
     public ReturnType getFeedback(String[] classList) {
-        //TODO: Change return types to ReturnType/ParsedData, whatever that ends up being.
         getRelevantData(classList);
         analyzeData();
-        ReturnType returnedMessages = composeReturnType();
-        return returnedMessages;
+        return composeReturnType();
     }
 
     public abstract void getRelevantData(String[] classList);
 
-    //TODO: Change x to ParsedData type.
     public abstract void analyzeData();
 
     public abstract ReturnType composeReturnType();
