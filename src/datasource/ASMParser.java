e6f9b0a1c98bc1ed736c3fc3f977f52d72ebd8b9
Alex Westerman <westeraj@rose-hulman.edu>
2022-02-10 13:58:35 -0500

    More work for Throwing Generic Exception #6
    -Added getMethodCaughtExceptions() to ASMParser
    -Added some defensive programming for ASMParser methods
    --Javadoc is updated as well
    -Added tests + common setup in ExceptionLinterTests
    -Added stub for ExceptionThrownAnalyzer implementation
    -VS code updated .project for some reason, changes seem innocuous

--- src/datasource/ASMParser.java
+++ src/datasource/ASMParser.java
@@ -1,7 +1,6 @@
 package datasource;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +8,7 @@ import java.util.Map;
 import org.objectweb.asm.*;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TryCatchBlockNode;
 
 public class ASMParser {
 
@@ -21,6 +21,8 @@ public class ASMParser {
             ClassNode decompiled = new ClassNode();
             reader.accept(decompiled, ClassReader.EXPAND_FRAMES);
         }
+
+        // This should be all we need for the parser to function
     }
 
     public String getUserFriendlyName(String className) {
@@ -43,11 +45,16 @@ public class ASMParser {
     /**
      * Returns a list of methods contained in the specified decompiled class.
      * 
+     * @throws IllegalArgumentException If the class specified is not being parsed
      * @param className The name of the class to retrieve methods from
      * @return An array of strings containing all names of the methods in the class.
      */
 
     public String[] getMethods(String className) {
+        if (!this.classMap.containsKey(className)) {
+            throw new IllegalArgumentException("Error! The specified class was not found in the parsed class map.");
+        }
+
         ClassNode decompiled = this.classMap.get(className);
         String[] result = null;
 
@@ -64,6 +71,8 @@ public class ASMParser {
     /**
      * Returns a list of exceptions that exist in the signature of a method
      * 
+     * @throws IllegalArgumentException If the method is not found in the
+     *                                  specified class
      * @param className  The name of the class where the method exists in
      * @param methodName The name of the method to retrieve the exceptions from
      * @return An array of strings containing the names of methods in the class.
@@ -87,6 +96,46 @@ public class ASMParser {
 
         String[] result = null;
         decompMethod.exceptions.toArray(result);
+        return result;
+    }
+
+    /**
+     * Returns a list of exceptions types that a method catches
+     * 
+     * @throws IllegalArgumentException If the method is not found in the specified
+     *                                  class
+     * @param className  The name of the class where the method should reside in
+     * @param methodName THe name of the method to retrieve caught exception types
+     *                   from
+     * @return A string array of internal types for each exception type that is
+     *         caught. Duplicates can occur
+     * 
+     */
+    public String[] getMethodExceptionCaught(String className, String methodName) {
+        ClassNode decompiled = this.classMap.get(className);
+
+        // Kinda have to find the method in the first place, use basic search for now
+        MethodNode decompMethod = null;
+        for (MethodNode node : decompiled.methods) {
+            if (node.name.equals("methodName")) {
+                decompMethod = node;
+            }
+        }
+
+        // Just a safeguard
+        if (decompMethod == null) {
+            throw new IllegalArgumentException("Error! Specified Method was not found in the class!");
+        }
+
+        List<TryCatchBlockNode> caughtExceptions = decompMethod.tryCatchBlocks;
+        List<String> caughtExceptionTypes = new ArrayList<>();
+
+        for (TryCatchBlockNode block : caughtExceptions) {
+            caughtExceptionTypes.add(block.type);
+        }
+
+        String[] result = null;
+        caughtExceptionTypes.toArray(result);
         return result;
     }
 
