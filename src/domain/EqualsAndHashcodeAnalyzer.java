7afa169fc82cc3cde546e69fac6eba4e89e668f0
rhit-boatmaee <boatmaee@gmail.com>
2022-02-17 01:49:18 -0500

    adding logic for analyzeData method

--- src/domain/EqualsAndHashcodeAnalyzer.java
+++ src/domain/EqualsAndHashcodeAnalyzer.java
@@ -4,19 +4,23 @@ import datasource.ASMParser;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EqualsAndHashcodeAnalyzer extends DomainAnalyzer {
 
 	ASMParser parser;
 	public Map<String, String[]> classAndMethodNames;
-	public Map<String, Set<String>> methodAndAnnotations;
-	public Map<String, Map<String, Set<String>>> classMethodsAndAnnotations;
+	List<LinterError> errorList = new ArrayList<>();
+	// public Map<String, Set<String>> methodAndAnnotations;
+	// public Map<String, Map<String, Set<String>>> classMethodsAndAnnotations;
 
 	public EqualsAndHashcodeAnalyzer(String[] classNames) {
 		try {
 			this.parser = new ASMParser(classNames);
+			this.classAndMethodNames = new HashMap<String, String[]>();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -24,25 +28,38 @@ public class EqualsAndHashcodeAnalyzer extends DomainAnalyzer {
 
 	@Override
 	public void getRelevantData(String[] classList) {
+		
 		for (String className : classList) {
+			System.out.println(className);
 			String[] methodNames = parser.getMethods(className);
-			Map<String, Set<String>> methodsAndAnnotations = new HashMap<>();
-			for (String methodName : methodNames) {
-				Set<String> methodAnnotations = parser.getMethodCompilerAnnotations(className, methodName);
-				methodsAndAnnotations.put(methodName, methodAnnotations);
-			}
-			classMethodsAndAnnotations.put(className, methodsAndAnnotations);
+			// Map<String, Set<String>> methodsAndAnnotations = new HashMap<>();
+			// for (String methodName : methodNames) {
+			// 	Set<String> methodAnnotations = parser.getMethodCompilerAnnotations(className, methodName);
+			// 	methodsAndAnnotations.put(methodName, methodAnnotations);
+			// }
+			// classMethodsAndAnnotations.put(className, methodsAndAnnotations);
+		classAndMethodNames.put(className, methodNames);
 		}
 
 	}
 
 	public void analyzeData() {
-
+		LinterError er = new LinterError("test error class name", "test error method name", "this is a test to verify what methods are in the class", ErrType.INFO);
+				this.errorList.add(er);
+		for (String className : classAndMethodNames.keySet()){
+			String[] methodNames = classAndMethodNames.get(className);
+			for(String methodName : methodNames){
+				System.out.println("A method in " + className + " is " + methodName);
+				LinterError err = new LinterError(className, methodName, "this is a test to verify what methods are in the class", ErrType.INFO);
+				this.errorList.add(err);
+			}
+		}
 	}
 
 	@Override
 	public ReturnType composeReturnType() {
-		return null;
+		ReturnType type = new ReturnType("Equals And Hashcode Override Check", this.errorList);
+		return type;
 	}
 
 }
--- src/presentation/PresentationLayer.java
+++ src/presentation/PresentationLayer.java
@@ -13,7 +13,7 @@ import domain.ExceptionThrownAnalyzer;
 import domain.LinterError;
 import domain.PrincipleOfLeastKnowledgeAnalyzer;
 import domain.ReturnType;
-import domain.SwitchCaseAnalyzer;
+import domain.EqualsAndHashcodeAnalyzer;
 import domain.VarNameAnalyzer;
 
 public class PresentationLayer {
@@ -46,6 +46,7 @@ public class PresentationLayer {
 
 		// Create analyzers and pass them through the list.
 		analyzers.add(new ExceptionThrownAnalyzer(parser));
+		analyzers.add(new EqualsAndHashcodeAnalyzer(classList));
 		// analyzers.add(new PrincipleOfLeastKnowledgeAnalyzer()); //TODO Update
 		// constructor if it chages
 		// analyzers.add(new VarNameAnalyzer(fileList)); //TODO Update constructors if
