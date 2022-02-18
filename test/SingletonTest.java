7713e5ce71b377683da5c603ae8c574c048e6160
rhit-boatmaee <boatmaee@gmail.com>
2022-02-17 20:37:46 -0500

    added SingletonTest file and started stubbing

---
+++ test/SingletonTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class SingletonTest {
+    
+    @Test
+    public void hasPublicConstructorStaticFieldStaticMethodTest(){
+        //This test shouldn't detect a singleton
+    }
+    @Test
+    public void hasPublicConstructorNoFieldStaticMethodTest(){
+        //this test shouldn't detect a singleton
+    }
+    @Test
+    public void hasPublicConstructorNoFieldNoStaticMethodTest(){
+        //this test shouldn't detect a singleton
+
+    }
+    @Test
+    public void hasPublicConstructorTest(){
+
+    }
+
+    
+}
