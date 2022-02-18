0f0080ebe1d294922839e8ad33f6641b1a17ff61
rhit-boatmaee <boatmaee@gmail.com>
2022-02-17 20:58:25 -0500

    implemented test classes with private constructors

--- src/example/singleton/PriConNoFieldNoMethod.java
+++ src/example/singleton/PriConNoFieldNoMethod.java
@@ -2,4 +2,12 @@ package example.singleton;
 
 public class PriConNoFieldNoMethod {
     
+    public String cat;
+    private PriConNoFieldNoMethod(){
+        cat = "Turbo!";
+    }
+
+    public String getCat(){
+        return this.cat;
+    }
 }
--- src/example/singleton/PriConNoFieldStaticMethod.java
+++ src/example/singleton/PriConNoFieldStaticMethod.java
@@ -2,4 +2,16 @@ package example.singleton;
 
 public class PriConNoFieldStaticMethod {
     
+    public String cat;
+    private PriConNoFieldStaticMethod(){
+        cat = "Turbo!";
+    }
+
+
+    public String getCat(){
+        return this.cat;
+    }
+    public static PriConNoFieldStaticMethod getInstance(){
+        return null;
+    }
 }
