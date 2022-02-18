7a84c39398ebee6bee7127a20eb2b3cad5f1835b
rhit-boatmaee <boatmaee@gmail.com>
2022-02-18 14:51:25 -0500

    added stubbing and TODOs for dryAnalyzer

--- src/domain/dryAnalyzer.java
+++ src/domain/dryAnalyzer.java
@@ -1,23 +1,43 @@
 package domain;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import datasource.ASMParser;
+
 public class dryAnalyzer extends DomainAnalyzer{
+
+    private ASMParser parser;
+	private List<LinterError> errors;
+    private String[] classList;
+
+    public dryAnalyzer(String[] classList){
+        try {
+			this.parser = new ASMParser(classList);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.errors = new ArrayList<>();
+    }
 
     @Override
     public void getRelevantData(String[] classList) {
-        // TODO Auto-generated method stub
-        
+        this.classList = classList;
+        // TODO: Need to get methods from all classes 
     }
 
     @Override
     public void analyzeData() {
-        // TODO Auto-generated method stub
+        // TODO: Need to check methods for duplication and 
+        // trigger an error if two methods have duplication
+
         
     }
 
     @Override
     public ReturnType composeReturnType() {
-        // TODO Auto-generated method stub
-        return null;
+        return new ReturnType("dryAnalyzer", this.errors);
     }
     
 }
