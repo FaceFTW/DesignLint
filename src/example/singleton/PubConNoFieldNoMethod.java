b32c92c50c93a8a6c675e4c9d658e3de6f9defab
rhit-boatmaee <boatmaee@gmail.com>
2022-02-17 21:01:44 -0500

    implemented test classes with public constructors

--- src/example/singleton/PubConNoFieldNoMethod.java
+++ src/example/singleton/PubConNoFieldNoMethod.java
@@ -1,5 +1,12 @@
 package example.singleton;
 
 public class PubConNoFieldNoMethod {
-    
+    public String cat;
+    public PubConNoFieldNoMethod(){
+        cat = "Turbo!";
+    }
+
+    public String getCat(){
+        return this.cat;
+    }
 }
--- src/example/singleton/PubConStaticFieldNoMethod.java
+++ src/example/singleton/PubConStaticFieldNoMethod.java
@@ -2,4 +2,14 @@ package example.singleton;
 
 public class PubConStaticFieldNoMethod {
     
+    
+    public String cat;
+    private static PubConStaticFieldNoMethod instt = new PubConStaticFieldNoMethod();
+    public PubConStaticFieldNoMethod(){
+        cat = "Turbo!";
+    }
+
+    public String getCat(){
+        return instt.getCat();
+    }
 }
