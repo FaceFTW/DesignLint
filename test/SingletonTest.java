6fb7192794b849302ae9c60d7f1c1a354a4ceed0
rhit-boatmaee <boatmaee@gmail.com>
2022-02-18 00:52:20 -0500

    added testForAllClasses

--- test/SingletonTest.java
+++ test/SingletonTest.java
@@ -10,39 +10,42 @@ public class SingletonTest {
     private SingletonAnalyzer analyzer;
 
     @Test
-    public void hasPublicConstructorStaticFieldStaticMethodTest(){
-        //This test shouldn't detect a singleton
-        String[] classList = {"example/singleton/PubConStaticFieldStaticMethod" };
+    public void hasPublicConstructorStaticFieldStaticMethodTest() {
+        // This test shouldn't detect a singleton
+        String[] classList = { "example/singleton/PubConStaticFieldStaticMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
         assertEquals("SingletonAnalyzer", returned.analyzerName);
         assertEquals(0, returned.errorsCaught.size());
     }
+
     @Test
-    public void hasPublicConstructorNoFieldStaticMethodTest(){
-        //this test shouldn't detect a singleton
-        String[] classList = {"example/singleton/PubConNoFieldStaticMethod" };
+    public void hasPublicConstructorNoFieldStaticMethodTest() {
+        // this test shouldn't detect a singleton
+        String[] classList = { "example/singleton/PubConNoFieldStaticMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
         assertEquals("SingletonAnalyzer", returned.analyzerName);
         assertEquals(0, returned.errorsCaught.size());
     }
+
     @Test
-    public void hasPublicConstructorStaticFieldNoMethodTest(){
-        //This test shouldn't detect a singleton
-        String[] classList = {"example/singleton/PubConStaticFieldNoMethod" };
+    public void hasPublicConstructorStaticFieldNoMethodTest() {
+        // This test shouldn't detect a singleton
+        String[] classList = { "example/singleton/PubConStaticFieldNoMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
         assertEquals("SingletonAnalyzer", returned.analyzerName);
         assertEquals(0, returned.errorsCaught.size());
     }
+
     @Test
-    public void hasPublicConstructorNoFieldNoMethodTest(){
-        //this test shouldn't detect a singleton
-        String[] classList = {"example/singleton/PubConNoFieldNoMethod" };
+    public void hasPublicConstructorNoFieldNoMethodTest() {
+        // this test shouldn't detect a singleton
+        String[] classList = { "example/singleton/PubConNoFieldNoMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
@@ -50,40 +53,44 @@ public class SingletonTest {
         assertEquals(0, returned.errorsCaught.size());
 
     }
+
     @Test
-    public void hasPrivateConstructorNoFieldStaticMethodTest(){
-        //This test shouldn't detect a singleton
-        String[] classList = {"example/singleton/PriConNoFieldStaticMethod" };
+    public void hasPrivateConstructorNoFieldStaticMethodTest() {
+        // This test shouldn't detect a singleton
+        String[] classList = { "example/singleton/PriConNoFieldStaticMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
         assertEquals("SingletonAnalyzer", returned.analyzerName);
         assertEquals(0, returned.errorsCaught.size());
     }
+
     @Test
-    public void hasPrivateConstructorNoFieldNoMethodTest(){
-        //This test shouldn't detect a singleton
-        String[] classList = {"example/singleton/PriConNoFieldNoMethod" };
+    public void hasPrivateConstructorNoFieldNoMethodTest() {
+        // This test shouldn't detect a singleton
+        String[] classList = { "example/singleton/PriConNoFieldNoMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
         assertEquals("SingletonAnalyzer", returned.analyzerName);
         assertEquals(0, returned.errorsCaught.size());
     }
+
     @Test
-    public void hasPrivateConstructorStaticFieldNoMethodTest(){
-        //This test shouldn't detect a singleton
-        String[] classList = {"example/singleton/PriConStaticFieldNoMethod" };
+    public void hasPrivateConstructorStaticFieldNoMethodTest() {
+        // This test shouldn't detect a singleton
+        String[] classList = { "example/singleton/PriConStaticFieldNoMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
         assertEquals("SingletonAnalyzer", returned.analyzerName);
         assertEquals(0, returned.errorsCaught.size());
     }
+
     @Test
-    public void hasPrivateConstructorStaticFieldStaticMethodTest(){
-        //This test SHOULD detect a singleton
-        String[] classList = {"example/singleton/PriConStaticFieldStaticMethod" };
+    public void hasPrivateConstructorStaticFieldStaticMethodTest() {
+        // This test SHOULD detect a singleton
+        String[] classList = { "example/singleton/PriConStaticFieldStaticMethod" };
         this.analyzer = new SingletonAnalyzer(classList);
         ReturnType returned = this.analyzer.getFeedback(classList);
 
@@ -92,10 +99,23 @@ public class SingletonTest {
         assertEquals("SingletonAnalyzer", returned.errorsCaught.get(0).className);
         assertEquals("Singleton Pattern detected!", returned.errorsCaught.get(0).message);
         assertEquals(ErrType.PATTERN, returned.errorsCaught.get(0).type);
-        
-        
-
     }
 
-    
+    @Test
+    public void allClassesTest() {
+        String[] classList = { "example/singleton/PriConStaticFieldStaticMethod",
+                "example/singleton/PriConStaticFieldNoMethod", "example/singleton/PriConNoFieldNoMethod",
+                "example/singleton/PriConNoFieldStaticMethod", "example/singleton/PubConNoFieldNoMethod",
+                "example/singleton/PubConStaticFieldNoMethod", "example/singleton/PubConNoFieldStaticMethod",
+                "example/singleton/PubConStaticFieldStaticMethod" };
+        this.analyzer = new SingletonAnalyzer(classList);
+        ReturnType returned = this.analyzer.getFeedback(classList);
+
+        assertEquals("SingletonAnalyzer", returned.analyzerName);
+        assertEquals(1, returned.errorsCaught.size());
+        assertEquals("SingletonAnalyzer", returned.errorsCaught.get(0).className);
+        assertEquals("Singleton Pattern detected!", returned.errorsCaught.get(0).message);
+        assertEquals(ErrType.PATTERN, returned.errorsCaught.get(0).type);
+    }
+
 }
